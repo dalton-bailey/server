e0d5469bba5a7e52492d6f21c4fde1703266a713
Bailey Dalton <baidalton@gmail.com>
2021-03-24 17:41:35 -0600

    mongoose connected

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+
+mongoose.set('useFindAndModify', false);
+
+dotenv.config()
 
 const port = process.env.PORT || 5050
 
 const app = express()
 
-const main = () => {
+const main = async () => {
+    await mongoose.connect(`${process.env.DGM4790_CONNECTION_STRING}`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+})
     app.listen(port, () => {
         console.log(`Listening at http://localhost:${port}`)
     })
